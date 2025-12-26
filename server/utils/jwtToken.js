2e9bcec5196a5fb2b4173315a373e8d84e55a9ea
sudharcruz <sudharsandevaraj1219@gmail.com>
2025-12-27 00:53:50 +0530

    session increased

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -3,7 +3,7 @@ const sendToken = (user, statusCode, res) => {
 
   const options = {
     expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 60 * 24 * 1000
+      Date.now() + process.env.COOKIE_EXPIRE * 60 * 60 * 24 * 1000
     ),
     httpOnly: true,
     sameSite: "lax",
